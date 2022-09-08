5aa2c2fb0eba3ef6ae90900b81b20f6832c84a20
nurannalci <nuran.nalann2411@gmail.com>
2022-09-08 16:28:57 +0200

    new

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: 27.04.2022 Lütfen Task.txt dosyasını okuyunuz.
+
+        System.out.println("merhaba");
     }
 }
